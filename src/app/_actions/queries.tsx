2dba34bc1566c8b1ba1658de7258d6079c28ce60
Vaibhav <vaibhav.p@xpuls.ai>
2024-06-15 02:00:24 +0530

    feat: add functionality to fetch data for both users

--- src/app/_actions/queries.tsx
+++ src/app/_actions/queries.tsx
@@ -34,19 +34,31 @@ export const getUserAddressFromFCUsername = async (username: string) => {
 
 export const getAllNFTsForAddress = async (address: string, client: CovalentClient) => {
     const resp = await client.NftService.getNftsForAddress("base-mainnet",`${address}`, {"withUncached": true});
-    console.log(resp.data);
+    console.log(`Fetching NFT data for user`);
     return resp.data;
 }
 
 export const getAllTokensForAddress = async (address: string, client: CovalentClient) => {
     const resp = await client.BalanceService.getTokenBalancesForWalletAddress("base-mainnet",`${address}`);
-    console.log(resp.data);
+    console.log(`Fetching token data for user`);
     return resp.data;
 }
 
-export const calculateSimilarity = async (username: string) => {
-    const address = await getUserAddressFromFCUsername(username);
+export const calculateSimilarity = async (primaryUsername: string, secondaryUsername: string) => {
+    const primaryAddress = await getUserAddressFromFCUsername(primaryUsername);
+    const secondaryAddress = await getUserAddressFromFCUsername(secondaryUsername);
+
     const client = new CovalentClient(`${process.env.COVALENT_API_KEY}`);
-    await getAllNFTsForAddress(address, client);
-    await getAllTokensForAddress(address, client);
+
+    const primaryNftData = await getAllNFTsForAddress(primaryAddress, client);
+    const secondaryNftData = await getAllNFTsForAddress(secondaryAddress, client);
+
+    const primaryTokenData = await getAllTokensForAddress(primaryAddress, client);
+    const secondaryTokenData = await getAllTokensForAddress(secondaryAddress, client);
+
+    // if(primaryNftData.items.length > 0) {
+    //     console.log(primaryNftData.items[0].nft_data);
+    // }
+
+    console.log(primaryTokenData);
 }
