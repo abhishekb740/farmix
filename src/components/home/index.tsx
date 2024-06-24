ce1be7792e284d14887b93a00b01287a54b5dbb4
Abhishek <abhishekb740@gmail.com>
2024-06-24 13:49:59 +0530

    style: Home page background image fixed

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -74,7 +74,7 @@ export default function Hero() {
         show={showNotification}
         onClose={() => setShowNotification(false)}
       />
-      <div className="w-full flex flex-row justify-center items-center">
+      <div className="w-full flex flex-row justify-center items-center mb-36">
         <div className="flex flex-row w-1/2 rounded-3xl py-1 px-5 items-center justify-center bg-white border shadow-[0_0_30px_#A675D8]">
           <input
             className="flex ml-4 w-full py-1.5 bg-transparent focus:outline-none text-black"
