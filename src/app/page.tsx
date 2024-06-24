a1ef80234e68ef049c951f536adadf95eecdfc20
Abhishek <abhishekb740@gmail.com>
2024-06-24 15:19:03 +0530

    feat: Only for desktop condition added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+"use client"
 import { Hero2 } from "@/components";
-import { NextUIProvider } from "@nextui-org/react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+    console.log(userAgent)
+    const mobile = /Mobi|Android/i.test(userAgent);
+    console.log(mobile);
+    
+    setIsMobile(mobile);
+  }, []);
+
   return (
     <div>
-      <Hero2 />
+      {
+        isMobile ?
+          <div className="flex flex-row justify-center items-center">
+            <div className="text-2xl">
+              Please open this website on a desktop.
+            </div>
+          </div> 
+          : <Hero2 />
+      }
     </div>
   );
 }
